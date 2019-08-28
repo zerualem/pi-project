0771f961ce7ef0118f59162f372920ed44a2a700
Zerihun <zerihuna@gmail.com>
2019-08-28 19:30:51 -0400

    Update impute-static.R

--- impute-static.R
+++ impute-static.R
@@ -1,4 +1,4 @@
 
 
-static_data <- read.csv("Analysis/static_agg_data.csv", header=T, row.names = 1)
+static_data <- read.csv("Data/static_agg_data.csv", header=T, row.names = 1)
 print(dim(static_data))
