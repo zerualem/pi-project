443239f8cdfa6e5a37aa6ad0da77f534a2ef3e2f
Zerihun <zerihuna@gmail.com>
2019-08-29 02:12:24 -0400

    Updated

--- impute-static.R
+++ impute-static.R
@@ -1,4 +1,21 @@
 
 
-static_data <- read.csv("Data/static_agg_data.csv", header=T, row.names = 1)
+
+
+static_data <- read.csv("Analysis/static_agg_data.csv", header=T, row.names = 1)
 print(dim(static_data))
+
+library(missForest)
+library(doParallel)
+
+registerDoParallel(cores = 6)
+
+imp_static <- missForest(static_data, maxiter = 3, ntree = 10, parallelize = "variable")
+
+imp_data <- imp_static$ximp
+
+print(imp_static$OOBerror)
+
+summary(imp_data)
+
+saveRDS(imp_data, file = "imp_static.rds")
