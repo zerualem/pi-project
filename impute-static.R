1c29e0236650a8f59235b4b8070c6650b06156e1
Zerihun <zerihuna@gmail.com>
2019-08-30 16:13:21 -0400

    Update impute-static.R

--- impute-static.R
+++ impute-static.R
@@ -30,17 +30,17 @@ library(mice)
 ## Define imputation function
 impute <- function (fname) {
   
-  raw_data <- read.csv(paste0("Analysis/",fname), header=T, row.names = 1)
+  raw_data <- read.csv(paste0("Analysis/",fname, ".csv"), header=T, row.names = 1)
   print(paste("Imputing", fname,"data"))
   print(dim(raw_data))
   imp_data <- mice(raw_data,m=3,maxit=5,seed=500)
-  saveRDS(imp_data, file = paste0(fname,"mice.rds"))
+  saveRDS(imp_data, file = paste0("Output/", fname,"mice.rds"))
 }
 
 ## Files list
-files.name <- c("static_agg_data.csv", "braden_agg_perc.csv", "location_agg.csv", 
-                "periop_numeric_agg.csv", "periop_yn_agg.csv", "surgical_data_agg.csv", 
-                "ts_binary_agg.csv", "ts_numeric_agg.csv")
+files.name <- c("static_agg_data", "braden_agg_perc", "location_agg", 
+                "periop_numeric_agg", "periop_yn_agg", "surgical_data_agg", 
+                "ts_binary_agg", "ts_numeric_agg")
 
 
 ## Impute all data
